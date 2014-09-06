7dfc5a9d1adca3649dab7f456332518b0b9b56d4
tangentstorm <michal.wallace@gmail.com>
2014-09-06 01:27:43 -0500

    cleaned up simplefpcdemo

--- simplefpcdemo.pas
+++ simplefpcdemo.pas
@@ -1,20 +1,19 @@
 // demo illustrating how to use python for delphi
 // from plain fpc, without lazarus or anything.
 
-{$mode delphi}
 program simplefpcdemo;
 uses PythonEngine, dynlibs;
 
-var eng : TPythonEngine; path : string;
+var eng : TPythonEngine;
 begin
   eng := TPythonEngine.Create(Nil);
-  eng.loaddll;
-  if eng.ishandlevalid then
+  eng.LoadDll;
+  if eng.IsHandleValid then
     begin
-      writeln(' evens: ', eng.EvalStringAsStr('[x*2 for x in range(10)]'));
+      WriteLn(' evens: ', eng.EvalStringAsStr('[x*2 for x in range(10)]'));
       eng.ExecString('print "powers:", [x**2 for x in range(10)]');
     end
-  else writeln('invalid library handle!', GetLoadErrorStr);
+  else writeln('invalid library handle!', dynlibs.GetLoadErrorStr);
 end.
 { --- output -----
 
