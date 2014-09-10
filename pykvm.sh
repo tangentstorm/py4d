4f283f9d276c7dc9b86a2187d156d0d15cf572ca
tangentstorm <michal.wallace@gmail.com>
2014-09-10 04:08:29 -0400

    pykvm.sh: make python2.7 explicit since i use different versions

--- pykvm.sh
+++ pykvm.sh
@@ -2,5 +2,5 @@ CORE=PythonForDelphi/Components/Sources/Core
 rm *.o *.so *.ppu
 fpc -Mdelphi -fPIC -gl -B -dPYTHON27 -Fu$CORE -Fi$CORE _pykvm.pas \
 && mv lib_pykvm.so kvm.so \
-&& python -ic 'import kvm; kvm.cwrite("|_|Bhello|g, |Bworld|g!|w|_|_")'
+&& python2.7 -ic 'import kvm; kvm.cwrite("|_|Bhello|g, |Bworld|g!|w|_|_")'
 
